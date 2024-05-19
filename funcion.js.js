a79c970e0f6dcb13b81e50cfcdd73b2226083efb
xzyAlejandroSMTP <sshxdevdiegoalejandrogo@gmail.com>
2024-05-19 19:14:56 +0100

    Add files via upload

---
+++ funcion.js.js
@@ -0,0 +1,21 @@
+var  ourCat  = "diegoalejandr"
+var ourDog = "Dog"
+
+var Im = " decod "; 
+var Mystr = "Im " +  ourCat + " i have perros or[ " [Mystr.lenght - 2 ];
+
+
+console.log(Mystr)
+var sf = awe = "well";
+var vl = 0 ;
+var whenlovemycat =  "More what the world  " [whenlovemycat + 4  ];
+console.log(vl, whenlovemycat);
+
+function wordBlanks(m , y , mv  ){
+var i = "adios ";
+rs = "";
+return rs;
+rs  +=" s" +'mundo';
+}
+
+console.log(wordBlanks("cruel","qsesio","malos", "del sat"))
