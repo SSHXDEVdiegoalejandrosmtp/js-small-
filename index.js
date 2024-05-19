473526cff0f12ba7814449780d65136acdd2bd58
xzyAlejandroSMTP <sshxdevdiegoalejandrogo@gmail.com>
2024-05-19 18:55:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var  ourCat  = "diegoalejandr"
+var ourDog = "Dog"
+
+var Im = " decod "; 
+var Mystr = "Im " +  ourCat + " i have perros or ";
+
+
+console.log(Mystr)
+var sf = awe = "well";
+var vl = 0 ;
+
+console.log(vl);
